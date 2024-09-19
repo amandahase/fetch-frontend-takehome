1530ff6b0aeff091132029b432cf2d0d2b36a9f7
Amanda Hasenzahl <ahasenzahl@blumira.com>
2024-09-19 16:30:19 -0600

    Add new folders and files for new page routes

---
+++ src/app/match/page.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function Match() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>YOU HAVE A MATCH!</h1>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/search/page.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function Search() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>HEY THERE!</h1>
+      </main>
+    </div>
+  );
+}
